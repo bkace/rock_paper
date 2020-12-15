127a57d356f27c3d3c6508604f29c97bca0e66cc
bikeshm <bksha8@gmail.com>
2020-12-15 20:18:52 +0200

    Change on master

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 from random import randint
 
 #create a list of play options
-t = ["Rock","Paper","Scissors"]
+option = ["Rock","Paper","Scissors"]
 
 #assign a random play to the computer
-computer = t[randint(0,2)]
+computer = option[randint(0,2)]
 
 #set player to False
 player = False
@@ -33,4 +33,4 @@ while player == False:
         print("That's not a valid play. Check your spelling!")
     #player was set to True, but we want it to be False so the loop continues 
     player = False
-    computer = t[randint(0,2)]
+    computer = option[randint(0,2)]
